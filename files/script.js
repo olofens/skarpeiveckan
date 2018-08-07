b6c0fb5d9183fbc1d8b8602e2c4af63ea2f58c51
olofens <olofens@student.chalmers.se>
2018-08-08 00:53:11 +0200

    add list items dynamically via javascript

---
+++ files/script.js
@@ -0,0 +1,8 @@
+function hello() {
+	
+	var newElem = document.getElementById("mall").cloneNode(true);
+	document.getElementById("mall").style.visibility = "hidden";
+	var list = document.getElementById("lista");
+	list.insertBefore(newElem, document.getElementById("mall"));
+}
+hello();
