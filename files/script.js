e677360b2756f98a0b1e06b6ff143dade67344ea
Olof Enström <olofens@student.chalmers.se>
2018-08-09 16:39:02 +0200

    ss

--- files/script.js
+++ files/script.js
@@ -1,8 +1,31 @@
-function hello() {
+function hello(matches) {
 	
-	var newElem = document.getElementById("mall").cloneNode(true);
-	document.getElementById("mall").style.visibility = "hidden";
-	var list = document.getElementById("lista");
-	list.insertBefore(newElem, document.getElementById("mall"));
+	var i;
+	for (i = 0; i < matches; i++) {
+		var newElem = document.getElementById("mall").cloneNode(true);
+		document.getElementById("mall").style.visibility = "hidden";
+		newElem.id = "mall" + i;
+		var list = document.getElementById("lista");
+		list.insertBefore(newElem, document.getElementById("mall"));
+		newElem.style.visibility = "";
+	}
 }
-hello();
+hello(5);
+
+function parsefile() {
+	var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", "file:///Users/olofenstrom/Desktop/skarpe/skarpeiveckan/files/matcher.txt", false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
+parsefile();
