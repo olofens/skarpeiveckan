e39f5c3d6e2e174df03eb5069651bb218a717d3f
olofens <olofens@student.chalmers.se>
2018-08-09 20:30:03 +0200

    nodejs now sends index.html to client

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 var http = require('http');
+var fs = require('fs');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
+    fs.readFile("./index.html", null, function(error, data) {
+    	if (error) {
+    		res.writeHead(404);
+    		res.write("File not found");
+    	} else {
+    		res.write(data);
+    	}
+    	res.end();
+    });
 }).listen(8080);
 
 function hello(matches) {
