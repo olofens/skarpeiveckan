d14f746556fa0a22ad1c53a03340c5ced63d9f90
olofens <olofens@student.chalmers.se>
2018-08-17 23:55:11 +0200

    start adding mongodb

--- skarpexpress/routes/index.js
+++ skarpexpress/routes/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require("mongodb").MongoClient;
+var assert = require("assert");
+
+var mongourl = "mongodb://localhost:27017";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-
-
   res.render('index', { title: 'Express' });
 });
 
