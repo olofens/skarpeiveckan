b04d95ecbcb6eedb4e9c8df097054f0a83f8e49a
olofens <olofens@student.chalmers.se>
2018-08-18 22:49:14 +0200

    website now pulls game data from mongodb and successfully renders in table rows

--- skarpexpress/routes/index.js
+++ skarpexpress/routes/index.js
@@ -1,14 +1,26 @@
 var express = require('express');
 var router = express.Router();
-var mongo = require("mongodb").MongoClient;
+var MongoClient = require("mongodb").MongoClient;
 var assert = require("assert");
 
-var mongourl = "mongodb://localhost:27017/test";
+var mongourl = "mongodb://localhost:27017/";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
-})
+  var resultArray = [];
+  MongoClient.connect(mongourl, function(err, db) {
+    if (err) throw err;
+    dbo = db.db("mydb");
+    var cursor = dbo.collection("väst-div2-games").find();
+    cursor.forEach(function(doc, err) {
+      if (err) throw err;
+      resultArray.push(doc);
+    }, function() {
+      db.close();
+      res.render('index', {games: resultArray});
+    });
+  });
+});
 
 
 module.exports = router;
