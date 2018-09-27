c35d1a5d29b441afe4262c5ad606d11bfa62ccc2
olofens <olofens@student.chalmers.se>
2018-09-27 15:49:17 +0200

    finally find a way to get react to work with my config

--- skarpexpress/public/javascripts/makeGames.js
+++ skarpexpress/public/javascripts/makeGames.js
@@ -1,4 +1,4 @@
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('root')
-);
+const name = "Olof";
+const element = <h1>Hello, {name}</h1>;
+const domContainer2 = document.querySelector('#hello_world_wrapper');
+ReactDOM.render(element, domContainer);
