d1355d9fb550d0a49414971118152b73fd79a88d
olofens <olofens@student.chalmers.se>
2018-09-27 17:15:13 +0200

    oh yea baby, render all games via reactjs!!!!!!!!!!

--- skarpexpress/public/javascripts/makeGames.js
+++ skarpexpress/public/javascripts/makeGames.js
@@ -1,9 +1,31 @@
 
 function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>
+  return <h1>Hello, {props.name} {props.name2}</h1>
 }
 
-const element = <Welcome name="Olle" />;
+function GameTable(props) {
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <td>{props.homeTeamName}</td>
+          <td>-</td>
+          <td>{props.awayTeamName}</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
+const element = <Welcome name="Olle" name2="Abbe" />;
+const gameElement = <GameTable homeTeamName = "ksk" awayTeamName = "ifk" />;
 
-ReactDOM.render(element, document.querySelector('#hello_world_wrapper'));
+const listItems = gamedata.map((gameobject) => <GameTable homeTeamName = {gameobject.homeTeamName}
+              awayTeamName = {gameobject.awayTeamName} />);
+
+
+
+
+ReactDOM.render(element, document.querySelector('#gamearea'));
+ReactDOM.render(gameElement, document.querySelector("#gamearea"));
+ReactDOM.render(<ul>{listItems}</ul>, document.querySelector("#gamearea"));
 console.log(gamedata);
