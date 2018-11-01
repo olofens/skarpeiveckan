91686f8d9c8a0123e9395d64fc6e6635b6922683
olofens <olofens@student.chalmers.se>
2018-11-01 16:05:07 +0100

    introduce optional makearena on startup

--- app.js
+++ app.js
@@ -732,6 +732,15 @@ function containsDayString(str) {
 // start database refreshing at a specific time. don't let the function be called twice via runningDone variable and 30.000s timer set
 function startBackend() {
     var runningDone = false;
+    MongoClient.connect(mongourl, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+            makeArena(dbo, "Skarpe Nord", function() {
+                console.log("Arena making done Skarpe");
+                db.close();
+            });
+        });
+    
     setInterval(function() {
         var date = new Date();
         if (date.getHours() === 2 && date.getMinutes() === 0 && !runningDone) {
